5b5534d3f88acd9d56b7d0ac38c744b8af9d689e
arronsh <arron0754_cn@126.com>
2023-06-11 21:41:49 +0800

    modify gpt icon

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -7,7 +7,7 @@ import { copyToClipboard, downloadAs, useMobileScreen } from "../utils";
 
 import CopyIcon from "../icons/copy.svg";
 import LoadingIcon from "../icons/three-dots.svg";
-import ChatGptIcon from "../icons/test.png";
+import ChatGptIcon from "../icons/170170.png";
 import ShareIcon from "../icons/share.svg";
 import BotIcon from "../icons/test.png";
 

75d8844ea658700f8e0eb64f2bf476cbff4b04cc
arronsh <arron0754_cn@126.com>
2023-06-11 17:38:37 +0800

    modify title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>AI小助手</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          做自己的AI助理.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
